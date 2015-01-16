07ab995639777fab7be4eda42a6d754f5f843353
Adam <adam.yee@xero.com>
2015-01-15 16:23:15 -0800

    reorganizing views.

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,29 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 router.get('/about', function (req, res) {
-  res.send(jade.renderFile('views/about.jade'));
+  res.send(jade.renderFile('views/home/about.jade'));
 });
 router.get('/code', function (req, res) {
-  res.send(jade.renderFile('views/code.jade'));
+  res.send(jade.renderFile('views/home/code.jade'));
 });
 router.get('/resume', function (req, res) {
-  res.send(jade.renderFile('views/resume.jade'));
+  res.send(jade.renderFile('views/home/resume.jade'));
 });
 
 router.get('/code/minesweeper', function (req, res) {
-  res.render('minesweeper');
+  res.render('code/minesweeper');
 });
 router.get('/code/canvas', function (req, res) {
-  res.render('canvas');
+  res.render('code/canvas');
 });
 router.get('/code/yelp', function (req, res) {
-  res.render('yelp');
+  res.render('code/yelp');
 });
 router.get('/code/messages-durandal', function (req, res) {
-  res.render('messages-durandal');
+  res.render('code/messages-durandal');
 });
 router.get('/code/messages-backbone', function (req, res) {
-  res.render('messages-backbone');
+  res.render('code/messages-backbone');
 });
 
 module.exports = router;
